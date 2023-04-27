e09f27222b4ac2140c4540cd4ea29fcfdae04d00
Divyaparvani <128139237+Divyaparvani@users.noreply.github.com>
2023-04-27 14:25:54 +0530

    Commit

---
+++ ITTicketManagement/ITMS.Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace ITMS.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext()
+           : base("Defaultconnection")
+        {
+        }
+    }
+}
---
+++ ITTicketManagement/ITMS.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITMS.WebUI.MvcApplication" Language="C#" %>
